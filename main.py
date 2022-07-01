c66feb0557065a522951a8702705c1c2bf9fbd0a
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-07-01 12:24:19 -0600

    scrape for upvoted articles

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from bs4 import BeautifulSoup
+import requests
 
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
 
+soup = BeautifulSoup(yc_web_page, 'html.parser')
 
+# all_titles = soup.find_all(name='a', class_='titlelink')
+# for title in all_titles:
+#     print(title.get('href'))
+
+articles = soup.find_all(name='a', class_='titlelink')
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get('href')
+    article_links.append(link)
+    
+'''See which articles have most upvotes'''
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name='span', class_='score')]
+# print(article_upvotes)
+most_votes = max(article_upvotes)
+mv_index = article_upvotes.index(most_votes)
+print(mv_index)
+print(article_upvotes[mv_index])
+print(article_texts[mv_index])
+print(article_texts[27])
+print(article_links[mv_index])
+'''Index off by 1 for links and texts'''
 
 
 
