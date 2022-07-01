efa05c817af09278a60440cc5a2f323d73f6684b
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-07-01 11:51:35 -0600

    local scraping general methods

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
+from bs4 import BeautifulSoup
 
 
-with open('website.html') as file:
-    contents = file.read()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# with open('website.html') as file:
+#     contents = file.read()
+    
+# soup = BeautifulSoup(contents, 'html.parser')
+# # print(soup.title)
+# # print(soup.title.name)
+# # print(soup.title.string)
+# # print(soup.prettify())
+# # print(soup.a)
+# all_anchor_tags = soup.find_all(name='a')
+# # print(all_anchor_tags)
+# # for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     # print(tag.get('href'))
+    
+# heading1 = soup.find(name='h1', id='name')
+# section_heading = soup.find(name='h3', class_='heading')
+# # print(section_heading.text)
+
+# company_url = soup.select_one(selector='p a')
+# name = soup.select_one(selector='#name')
+# headings = soup.select('.heading')
+# # print(headings)
+
